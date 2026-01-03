4f75093891adf120a525f6ca8b6e109e577ab93f
Adele-hhhh <adelehhh@outlook.fr>
2026-01-03 10:16:21 +0100

    Update main.py
    y'avait la class Grille en double donc j'en ai supprimé une et j'ai modifié les tailles pour que ce soit plus propre (c'est pas très important)

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class FenetrePrincipale:
         # Paramètres de la fenêtre
         master.title("Picross")
         master.geometry("900x900")
-        master.resizable(False, False)
+        master.resizable(False, False) 
         master.configure(bg="#FFFFFF")
 
         # Forcer la fenêtre au premier plan
@@ -28,54 +28,12 @@ class Grille_de_jeux:
         self.master = master
 
         # Canvas
-        self.canvas = tk.Canvas(master, width=650, height=650, bg="black")
-        self.canvas.place(x=125, y=180)
+        self.canvas = tk.Canvas(master, width=500, height=500, bg="black")
+        self.canvas.place(x=200, y=100)
 
         # Valeur par défaut
         self.cases = 10
-        self.taille_case = 650 // self.cases
-
-        # Dessin initial
-        self.dessiner_grille()
-
-    # --- FONCTION CLÉ ---
-    def set_cases(self, nb_cases):
-        """
-        Cette fonction sera appelée par un bouton
-        pour changer le nombre de cases.
-        """
-        self.cases = nb_cases
-        self.taille_case = 650 // self.cases
-
-        # Nettoyage du canvas
-        self.canvas.delete("all")
-
-        # Redessin
-        self.dessiner_grille()
-
-    def dessiner_grille(self):
-        for col in range(self.cases):
-            for lig in range(self.cases):
-                x1 = col * self.taille_case
-                y1 = lig * self.taille_case
-                x2 = x1 + self.taille_case
-                y2 = y1 + self.taille_case
-
-                self.canvas.create_rectangle(
-                    x1, y1, x2, y2,
-                    outline="gray"
-                )
-class Grille_de_jeux:
-    def __init__(self, master):
-        self.master = master
-
-        # Canvas
-        self.canvas = tk.Canvas(master, width=600, height=600, bg="black")
-        self.canvas.place(x=250, y=250)
-
-        # Valeur par défaut
-        self.cases = 5
-        self.taille_case = 600 // self.cases
+        self.taille_case = 500 // self.cases
 
         # Dessin initial
         self.dessiner_grille()
