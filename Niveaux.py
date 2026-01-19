1d2cb65b02b77e2de9b3ce0dfafc9c7d20717dce
Adele-hhhh <adelehhh@outlook.fr>
2026-01-19 22:16:13 +0100

    Niveaux = fusion d

---
+++ Niveaux.py
@@ -0,0 +1,59 @@
+import random 
+import NOMBRES as nb 
+
+def creation_liste(self):
+    return [[random.randint(0, 1) for _ in range(self.cases)]
+        for _ in range(self.cases)]
+
+def verifier_victoire(self):
+    """Vérifie si toutes les cases correspondent à la solution"""
+    for (col, lig), case in self.etats_cases.items():
+        solution = self.liste_solution[lig][col]
+        if case["etat"] == solution:
+            continue 
+        else : 
+            return False
+    return True 
+
+def rejouer(self): 
+    #nouvelles vies
+    self.nb_vies = 3
+    self.label_vies.config(text=f"Vies : {self.nb_vies}")#actualise l'affichage du nombre de vies
+
+    #--on enlève les cases qui ont été coloriées--
+
+    self.cases = self.nb_cases_cote.get() #on récupère le nombre de cases par côté dans le scale
+    self.taille_case = 500 // self.cases #calcule la taille d'une case
+
+    # Nettoyage du canvas
+    self.canvas.delete("all") #enlève la grille qu'il y avait avant
+    self.etats_cases.clear() #toutes les cases sont remises à 0
+        
+    # Redessin
+    self.dessiner_grille()    #remet une grille avec le nombre de cases demandé
+
+
+def nouveau_niveau(self):
+    """
+    Crée un nouveau niveau avec le nombre de cases demandé dans le Scale
+    """
+    self.cases = self.nb_cases_cote.get()#on récupère le nombre de cases par côté dans le scale
+    self.taille_case = 500 // self.cases    #calcule la taille d'une case
+
+    #nouvelle solution 
+    self.liste_solution = creation_liste(self) #on crée une nouvelle solution
+
+    #nouvelles vies
+    self.nb_vies = 3
+    self.label_vies.config(text=f"Vies : {self.nb_vies}")#actualise l'affichage du nombre de vies
+        
+    # Nettoyage du canvas
+    self.canvas.delete("all") #enlève la grille qu'il y avait avant
+    self.etats_cases.clear() #toutes les cases sont remises à 0
+        
+    # Redessin
+    self.dessiner_grille()    #remet une grille avec le nombre de cases demandé
+
+    #on affiche les nouveaux indices : 
+    nb.afficher_nb_col(self)  
+    nb.afficher_nb_lig(self)
