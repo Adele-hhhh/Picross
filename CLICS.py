ecda1919c9a915f367c9a3b7ce25e8ca2373dc9b
Adele-hhhh <adelehhh@outlook.fr>
2026-01-19 23:39:35 +0100

    ajout de vérifier victoire

--- CLICS.py
+++ CLICS.py
@@ -1,5 +1,14 @@
 from tkinter import messagebox 
-import Victoire_solution as vs 
+
+def verifier_victoire(self):
+    """Vérifie si toutes les cases correspondent à la solution"""
+    for (col, lig), case in self.etats_cases.items():
+        solution = self.liste_solution[lig][col]
+        if case["etat"] == solution:
+            continue 
+        else : 
+            return False
+    return True 
 
 #clic gauche : remplir une case
 def clic_case_gauche(event, self): 
@@ -8,12 +17,12 @@ def clic_case_gauche(event, self):
     lig = event.y // self.taille_case
 
     case = self.etats_cases[(col, lig)] 
-    if vs.verifier_victoire(self) == False:
+    if verifier_victoire(self) == False:
         if case["etat"] == 0:   #si la case est vide 
             if self.liste_solution[lig][col] == 1 and self.nb_vies > 0:#si c'est la bonne réponse et qu'on a encore des vies
                 self.canvas.itemconfig(case["rect"], fill="black") #on remplit la case
                 case["etat"] = 1 # on met à jour l'état de la case
-                if vs.verifier_victoire(self):
+                if verifier_victoire(self):
                     messagebox.showinfo(None, "Vous avez gagnééé !")
                     return
             else : 
@@ -29,7 +38,7 @@ def clic_case_droit(event, self):
     lig = event.y // self.taille_case
 
     case = self.etats_cases[(col, lig)]
-    if vs.verifier_victoire(self) == False:
+    if verifier_victoire(self) == False:
         if case["etat"] == 0:  # Si la case est déja remplie on met rien
             if self.liste_solution[lig][col] == 0 and self.nb_vies > 0 :#si c'est la bonne réponse et qu'on a encore des vies
                 #on dessine une croix dans la case :
@@ -42,7 +51,7 @@ def clic_case_droit(event, self):
                 l1 = self.canvas.create_line(x1+3, y1+3, x2-3, y2-3, fill="Violet red", width=2)
                 l2 = self.canvas.create_line(x1+3, y2-3, x2-3, y1+3, fill="Violet red", width=2)
                 case["etat"] = 0
-                if vs.verifier_victoire(self):
+                if verifier_victoire(self):
                     messagebox.showinfo(None, "Vous avez gagnééé !")
                     return
             else : 
