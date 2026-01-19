39021de4a9797001dddc603d0ce0a17f16f68fb5
Adele-hhhh <adelehhh@outlook.fr>
2026-01-19 23:45:38 +0100

    commentaires_finaux

--- Niveaux.py
+++ Niveaux.py
@@ -1,11 +1,14 @@
 import random 
 import NOMBRES as nb 
 
+#crée une liste de 0 et 1 aléatoire, les 1 représeteront les cases remplies et les 0 les cases vides
 def creation_liste(self):
     return [[random.randint(0, 1) for _ in range(self.cases)]
         for _ in range(self.cases)]
 
+#permet de recommencer la partie en cours (ou perdue)
 def rejouer(self): 
+    
     #nouvelles vies
     self.nb_vies = 3
     self.label_vies.config(text=f"Vies : {self.nb_vies}")#actualise l'affichage du nombre de vies
@@ -18,15 +21,12 @@ def rejouer(self):
     # Nettoyage du canvas
     self.canvas.delete("all") #enlève la grille qu'il y avait avant
     self.etats_cases.clear() #toutes les cases sont remises à 0
-        
+    
     # Redessin
     self.dessiner_grille()    #remet une grille avec le nombre de cases demandé
 
-
+#Crée un nouveau niveau avec le nombre de cases demandé dans le Scale
 def nouveau_niveau(self):
-    """
-    Crée un nouveau niveau avec le nombre de cases demandé dans le Scale
-    """
     self.cases = self.nb_cases_cote.get()#on récupère le nombre de cases par côté dans le scale
     self.taille_case = 500 // self.cases    #calcule la taille d'une case
 
@@ -45,6 +45,5 @@ def nouveau_niveau(self):
     self.dessiner_grille()    #remet une grille avec le nombre de cases demandé
 
     #on affiche les nouveaux indices : 
-    nb.afficher_nb_col(self)  
-
+    nb.afficher_nb_col(self)
     nb.afficher_nb_lig(self)
