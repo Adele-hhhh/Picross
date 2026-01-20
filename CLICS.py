2a3c0e5b98261c511173e853f19f13718e2174d5
Enoki <matis2008zhang@gmail.com>
2026-01-20 01:15:10 +0100

    Click ok

--- CLICS.py
+++ CLICS.py
@@ -1,49 +1,62 @@
-import random 
-import NOMBRES as nb 
+from tkinter import messagebox 
 
-#crée une liste de 0 et 1 aléatoire, les 1 représenteront les cases remplies et les 0 les cases vides
-def creation_liste(self):
-    return [[random.randint(0, 1) for _ in range(self.cases)]
-        for _ in range(self.cases)]
+def verifier_victoire(self):
+    """Vérifie si toutes les cases correspondent à la solution"""
+    for (col, lig), case in self.etats_cases.items():
+        solution = self.liste_solution[lig][col]
+        if case["etat"] == solution:
+            continue 
+        else : 
+            return False
+    return True 
 
-#permet de recommencer la partie en cours (ou perdue)
-def rejouer(self): 
-    
-    #nouvelles vies
-    self.nb_vies = 3
-    self.label_vies.config(text=f"Vies : {self.nb_vies}")#actualise l'affichage du nombre de vies
+#clic gauche : remplir une case
+def clic_case_gauche(event, self): 
+    #coordonnÃ©es de la case cliquÃ©e
+    col = event.x // self.taille_case 
+    lig = event.y // self.taille_case
 
-    #--on enlève les cases qui ont été coloriées--
+    case = self.etats_cases[(col, lig)] 
+    if verifier_victoire(self) == False:
+        if case["etat"] == 0:   #si la case est vide 
+            if self.liste_solution[lig][col] == 1 and self.nb_vies > 0:#si c'est la bonne réponse et qu'on a encore des vies
+                self.canvas.itemconfig(case["rect"], fill="black") #on remplit la case
+                case["etat"] = 1 # on met à jour l'état de la case
+                if verifier_victoire(self):
+                    messagebox.showinfo(None, "Vous avez gagnééééé !")
+                    return
+            else : 
+                if self.nb_vies >= 1: #si on a encore des vies on en enlève une
+                    self.nb_vies -= 1 
+                    self.label_vies.config(text=f"Vies : {self.nb_vies}") #on change le nombre affiché
+                    if self.nb_vies == 0:  # si on a plus de vies on perd
+                        messagebox.showinfo(None, "Vous avez perdu")
 
-    self.cases = self.nb_cases_cote.get() #on récupère le nombre de cases par côté dans le scale
-    self.taille_case = 500 // self.cases #calcule la taille d'une case
 
-    # Nettoyage du canvas
-    self.canvas.delete("all") #enlève la grille qu'il y avait avant
-    self.etats_cases.clear() #toutes les cases sont remises à 0
-    
-    # Redessin
-    self.dessiner_grille()    #remet une grille avec le nombre de cases demandé 
+def clic_case_droit(event, self):
+    col = event.x // self.taille_case 
+    lig = event.y // self.taille_case
 
-#Crée un nouveau niveau avec le nombre de cases demandé dans le Scale
-def nouveau_niveau(self):
-    self.cases = self.nb_cases_cote.get()#on récupère le nombre de cases par côté dans le scale
-    self.taille_case = 500 // self.cases    #calcule la taille d'une case
-
-    #nouvelle solution 
-    self.liste_solution = creation_liste(self) #on crée une nouvelle solution
-
-    #nouvelles vies
-    self.nb_vies = 3
-    self.label_vies.config(text=f"Vies : {self.nb_vies}")#actualise l'affichage du nombre de vies
-        
-    # Nettoyage du canvas
-    self.canvas.delete("all") #enlève la grille qu'il y avait avant
-    self.etats_cases.clear() #toutes les cases sont remises à 0
-        
-    # Redessin
-    self.dessiner_grille()    #remet une grille avec le nombre de cases demandé
-
-    #on affiche les nouveaux indices : 
-    nb.afficher_nb_col(self)
-    nb.afficher_nb_lig(self)
+    case = self.etats_cases[(col, lig)]
+    if verifier_victoire(self) == False:
+        if case["etat"] == 0:  # Si la case est déjà remplie on met rien
+            if self.liste_solution[lig][col] == 0 and self.nb_vies > 0 :#si c'est la bonne réponse et qu'on a encore des vies
+                #on dessine une croix dans la case :
+                x1 = col * self.taille_case
+                y1 = lig * self.taille_case
+                x2 = x1 + self.taille_case
+                y2 = y1 + self.taille_case
+            
+                # deux lignes en diagonale
+                l1 = self.canvas.create_line(x1+3, y1+3, x2-3, y2-3, fill="Violet red", width=2)
+                l2 = self.canvas.create_line(x1+3, y2-3, x2-3, y1+3, fill="Violet red", width=2)
+                case["etat"] = 0
+                if verifier_victoire(self):
+                    messagebox.showinfo(None, "Vous avez gagnééééé !")
+                    return
+            else : 
+                if self.nb_vies >= 1 : #si on a encore des vies on en enlève une
+                    self.nb_vies -= 1 
+                    self.label_vies.config(text=f"Vies : {self.nb_vies}") #on change le nombre affiché
+                    if self.nb_vies == 0 :  #si on a plus de vies on perd
+                        messagebox.showinfo(None, "Vous avez perdu")
